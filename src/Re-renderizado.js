381de88753387611f062db8eaa9115d9e17dd02b
vladius <vladpsico@gmail.com>
2023-02-09 13:05:48 -0600

    cambios

---
+++ src/Re-renderizado.js
@@ -0,0 +1,15 @@
+const App = (props) => {
+    const {counter} = props
+    return (
+        <div>
+            {counter}
+        </div>
+    )
+}
+
+let counter = 1
+
+ReactDOM.render(
+    <App counter = {counter} />,
+    document.getElementById('root')
+)
